995185a03afad4bcf27ad174c5e63e669676d759
John Mazzucco <John.Mazzucco@gmail.com>
2015-03-31 14:40:37 -0400

    committing exercise2

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+# 1 -tip calculator
+meal_amount = 55
+meal_tip = meal_amount * 0.20 
+puts "You left a $#{meal_tip} tip on your meal. Thanks!"
+
+# 2 -adding a string and int
+puts "almonds" + 2.to_s
+
+# 3 -mutliple numbers with string interpolation
+puts "#{45628 * 7839}"
+
+# 4 - booleans and operators
+# expression should return True since !(false && false) returns true, and the or operator only needs one argument to be true
+puts (true && false) || (false && true) || !(false && false)
+
